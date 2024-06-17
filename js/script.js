5728c830590b6c84b292cabfadfba90e669e58dd
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-17 16:02:56 +0200

    Feat: array and timer are now visible in DOM

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 // Visualizzare in pagina 5 numeri casuali.
 const randomNumbers = genSequenzaNumRandom(0,10,5)
 console.log(randomNumbers);
+//Richiamo la lista e il timer
+const randomArray = document.querySelector(".randomArray");
+const timerDisplay = document.querySelector(".timer");
+//Aggiungo l'array in pagina
+randomArray.innerHTML = `Memorizza i numeri: <br> ${randomNumbers}`;
+
 // Da lì parte un timer di 30 secondi.
-let seconds = 30;
+let seconds = 4;
 let timer = setInterval(countdown,1000);
+
+
 // Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
@@ -13,7 +21,8 @@ let timer = setInterval(countdown,1000);
 
 //FUNZIONI
 function countdown(){
-    console.log(seconds);
+    //Aggiungo il timer in pagina
+    timerDisplay.innerHTML = `Tempo rimanente: ${seconds}`;
     if(seconds > 0) {
         seconds--;
     }else {
